9d488fdefac1a4c7c2520c108e6a8a888db24aef
tannerfunk <88557372+tannerfunk@users.noreply.github.com>
2022-08-25 16:38:50 -0600

    Successful Connection
    Having issues on my setData function from the useState hook.

--- client/src/App.js
+++ client/src/App.js
@@ -4,17 +4,17 @@ import './App.css';
 import axios from 'axios';
 
 function App() {
-  const [data, setData] = useState([ ]);
+  const [data, setData] = useState([]);
 
   useEffect(() => {
     const user = `${config.apiBaseUrl}/courses`;
     axios.get(user)
-      .then(response => setData(response.data.data), console.log('success connecting to your REST API!'))
+      .then(response => setData(response.data.courses), console.log('success connecting to your REST API!'))
       .catch(error => console.log('Error fetching and parsing data', error))
   }, []);
   return (
     <ul>
-      <li>{ data }</li>
+      <li>{ data.title }</li>
     </ul>
   );
 }
